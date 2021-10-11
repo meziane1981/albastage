2252c75488fbfd00a65bc63ab2b37a7986ce9058
meziane1981 <meziane_maamar@yahoo.fr>
2021-10-11 15:52:56 +0200

    envoie de 11/10/2021

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5f65841cd8fa23cd837082d1b0acec9a7a1083f2',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5f65841cd8fa23cd837082d1b0acec9a7a1083f2',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.98.0',
+            'version' => '7.98.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => '87f27d3da4a72c93da49d81011cb675c2847a7c0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
